138a5612de3427f84e16ce588bd0ef55bb1f6b83
parindersinghraina <parindersingh@Parinders-MacBook-Pro.local>
2022-01-26 15:02:59 -0500

    add casting lecture in the course

--- src/main/java/com/parinder/learn/java/course/lecture7/Casting.java
+++ src/main/java/com/parinder/learn/java/course/lecture7/Casting.java
@@ -1,11 +1,19 @@
 package com.parinder.learn.java.course.lecture7;
 
 /*
+Casting means to treat or convert a number from one type to another. We put the type we want the number to be in parenthesis
+like this:
 
+(byte) (myMinByteValue/2)
+
+Other language have casting, this is not just Java.
  */
 public class Casting {
 
     public static void main(String[] args) {
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myNewByteValue = (byte) (myMinByteValue / 2); // By default, under parenthesis() java consider the number as Integer.
 
+        System.out.println("Casted Byte Value is : " + myMinByteValue);
     }
 }
